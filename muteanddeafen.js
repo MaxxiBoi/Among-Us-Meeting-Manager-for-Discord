5b2387946e695397a80938be6ca40b65199a66f7
Maxim Chintalov <max.chintalov@gmail.com>
2020-09-14 16:01:06 -0400

    Update muteanddeafen.js

--- muteanddeafen.js
+++ muteanddeafen.js
@@ -1,19 +1,20 @@
 // First, import the bot from the bot.js file
 var bot = require("./bot");
 
-module.exports = function (id, ghostsArray, boolean) {
-	if (boolean !== false) boolean = true;
-	bot.getChannel(id).voiceMembers.map(function (member) {
-		if (ghostsArray.includes(member.user)) {
+// Export the function
+module.exports = function (id, ghostsArray, boolean) { // id is ID of voice channel, ghostsArray is an array containing the user objects of the ghosts
+	if (boolean !== false) boolean = true; // If it doesn't say to explicitly end the meeting, start it
+	bot.getChannel(id).voiceMembers.map(function (member) { // Get the VoiceChannel that they're in and run the function on all Member objects
+		if (ghostsArray.includes(member.user)) { // Check to see if Member is a ghost
 			member.edit({
-				mute: boolean,
+				mute: boolean, // Ghosts are muted during meetings and unmuted when they end
 			}, "This is a ghost.");
 		} else {
 			member.edit({
-				mute: !boolean,
-				deaf: !boolean
+				mute: !boolean, // The living are unmuted during meetings and muted when they end
+				deaf: !boolean // The living are undeafened during meetings and deafened when they end
 			}, "This is not a ghost.");
 		}
-		return member.id;
+		return member.id; // I used this for debugging
 	});
 }
