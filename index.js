e2a21cc0fc7cc50b6a3df05e0c9227a0b3f6d1ec
Maxim Chintalov <max.chintalov@gmail.com>
2020-09-14 15:57:00 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,21 +1,24 @@
-var bot = require("./bot.js");
-var startmeet = require("./muteanddeafen.js");
-var endmeet = function (id, ghostsArray) {startmeet(id, ghostsArray, false);}
-var ghosts = new Array();
+// This is the file that Node JS should run
+// To start the bot, run ./index.js in this directory
 
-bot.on("messageCreate", function (msg) {
-	var ID = msg.member.voiceState.channelID;
-	if(msg.content === "!ping") {
+var bot = require("./bot.js"); // Import the bot from the bot.js file
+var startmeet = require("./muteanddeafen.js"); // Import the function from the muteanddeafen.js file
+var endmeet = function (id, ghostsArray) {startmeet(id, ghostsArray, false);} // startmeet with a false boolean ends the meeting, so use that for endmeet
+var ghosts = new Array(); // Make the array here so it doesn't get reset every time bot.on runs
+
+bot.on("messageCreate", function (msg) { // Event to be run every time a message is sent
+	var ID = msg.member.voiceState.channelID; // Save the channel ID of the message sender
+	if(msg.content === "!ping") { // This is from the get started code on the Eris Web site
 		bot.createMessage(msg.channel.id, "Pong!");
-	} else if (msg.content === "$startmeet") {
+	} else if (msg.content === "$startmeet") { // The command to start a meeting
 		startmeet(ID, ghosts);
-		console.log(ghosts);
-	} else if (msg.content === "$endmeet") {
+		console.log(ghosts); // This was here for debugging purposes
+	} else if (msg.content === "$endmeet") { // The command to end a meeting
 		endmeet(ID, ghosts);
-	} else if (msg.content.startsWith("$addghost")) {
-		ghosts = ghosts.concat(msg.mentions);
-		console.log(ghosts);
+	} else if (msg.content.startsWith("$addghost")) { // Add a ghost to the array
+		ghosts = ghosts.concat(msg.mentions); // Combine the ghosts array with the array containing all users mentioned in the command
+		console.log(ghosts); // This was here for debugging purposes
 	}
 });
 
-bot.connect();
+bot.connect(); // Connect to server
